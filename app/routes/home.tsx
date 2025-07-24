5c90cfdd2ce1cce8a7ff39a791048839146d259d
AmareshKoti <amareshkoti2005@gail.com>
2025-07-24 23:05:24 +0530

    Push local changes to GitHub

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/home";
 import {Fragment} from "react";
+import ResumeCard from "../../components/ResumeCard";
+import Navbar from "../../components/Navbar";
+import {resumes} from "../../constants";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "RESUMIND" },
@@ -9,11 +12,19 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return<main className="bg-[url('/images/bg-main.svg')] bg-cover" >
+    <Navbar/>
   <section className="main-section">
     <div className="page-heading">
       <h1>Track Your Application & Resume Ratings</h1>
       <h2>Review your submissions and check AI-powered feedback.</h2>
     </div>
   </section>
+    {resumes.length>0 &&(
+    <div className="resume-section">
+    {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume}/>
+    ))}
+    </div>
+        )}
   </main>;
 }
